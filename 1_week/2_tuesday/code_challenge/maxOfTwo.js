c14aa712921e749cb66820d543e802c0d9422d10
sohostory <sohostory@gmail.com>
2023-04-04 16:04:10 -0500

    Initial Commit

---
+++ 1_week/2_tuesday/code_challenge/maxOfTwo.js
@@ -0,0 +1,13 @@
+// MAX OF TWO
+
+let x = 1;
+let y = 2;
+
+// Write your code below this line.
+
+console.log("x: ", x);
+console.log("y: ", y);
+
+if (x === y) {
+  console.log("The values are identical.");
+}
