97497fe3f3dbbbdc83da57a4e25a13f8268183ec
sohostory <sohostory@gmail.com>
2023-04-11 09:53:37 -0500

    code challenge for today

--- 2_week/2_tuesday/code_challenge/findLuckyNumbers.js
+++ 2_week/2_tuesday/code_challenge/findLuckyNumbers.js
@@ -1,1 +1,17 @@
 // Write your code below this line.
+
+const luckyNumbers = (num) => {
+  let luckyArray = [];
+
+  while (luckyArray.length < num) {
+    const number = Math.floor(Math.random() * 10) + 1;
+    if (!luckyArray.includes(number)) {
+      luckyArray.push(number);
+    }
+  }
+
+  return luckyArray;
+};
+
+let luckyNums = luckyNumbers(4);
+console.log(luckyNums);
