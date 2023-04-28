b93efdda66f442720d342d5a69c62cacbdd0a487
sohostory <sohostory@gmail.com>
2023-04-27 20:19:40 -0500

    demo

---
+++ 4_week/student_demo/server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(4000, () => console.log("server is listening on port 4000"));
