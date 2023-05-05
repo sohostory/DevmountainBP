0f22adada8d8fc95075891e5055dccc8f9fcd796
sohostory <sohostory@gmail.com>
2023-05-04 20:55:17 -0500

    react robot test app

---
+++ 5_week/4_thursday/specialization/react/robots/src/components/Form.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <form onSubmit={props.handleRobotAdd}>
+      <p>What is your new robot's name?</p>
+      <input
+        type="text"
+        placeholder="name"
+        onChange={props.handleInput}
+        value={props.input}
+      />
+      <button>Create Robot</button>
+    </form>
+  );
+};
+
+export default Form;
